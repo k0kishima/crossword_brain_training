2eae3c2d28a52d863428ae9caa1d7bc00a649baa
Johndoe <56298669+togane@users.noreply.github.com>
2021-08-22 08:06:07 +0900

    add the store

---
+++ src/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  useSelector as rawUseSelector,
+  TypedUseSelectorHook,
+} from 'react-redux';
+
+export const store = configureStore({
+  reducer: {
+    //question: questionSlice.reducer,
+  },
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+// c.f. https://future-architect.github.io/articles/20200501/
+export const useSelector: TypedUseSelectorHook<RootState> = rawUseSelector;
