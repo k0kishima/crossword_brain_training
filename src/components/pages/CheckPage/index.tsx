14e80d4929d09df0f4e3c2e6164fcac094cdd468
Johndoe <56298669+togane@users.noreply.github.com>
2022-01-05 18:52:24 +0900

    fix how to make the result

--- src/components/pages/CheckPage/index.tsx
+++ src/components/pages/CheckPage/index.tsx
@@ -6,30 +6,30 @@ import { ResultTable } from './ResultTable/Presentation';
 import { NextButtonContainer } from './NextButton/Container';
 import { CHECK_RESULT } from 'types';
 
-const { question, answer } = store.getState();
-
-const results = question.cells.map((questionCellRow, i) => {
-  return questionCellRow.map((questionCell, j) => {
-    const answerCell = answer.cells[i][j];
-
-    if (questionCell && answerCell) {
-      return CHECK_RESULT.CORRECT_FILLED;
-    }
-    if (!questionCell && !answerCell) {
-      return CHECK_RESULT.CORRECT_BLANK;
-    }
-    if (questionCell && !answerCell) {
-      return CHECK_RESULT.NOT_FILLED;
-    }
-    if (!questionCell && answerCell) {
-      return CHECK_RESULT.WRONG_FILLED;
-    }
-
-    return CHECK_RESULT.UNKNOWN;
-  });
-});
-
 export const CheckPage: React.VFC = () => {
+  const { question, answer } = store.getState();
+
+  const results = question.cells.map((questionCellRow, i) => {
+    return questionCellRow.map((questionCell, j) => {
+      const answerCell = answer.cells[i][j];
+
+      if (questionCell && answerCell) {
+        return CHECK_RESULT.CORRECT_FILLED;
+      }
+      if (!questionCell && !answerCell) {
+        return CHECK_RESULT.CORRECT_BLANK;
+      }
+      if (questionCell && !answerCell) {
+        return CHECK_RESULT.NOT_FILLED;
+      }
+      if (!questionCell && answerCell) {
+        return CHECK_RESULT.WRONG_FILLED;
+      }
+
+      return CHECK_RESULT.UNKNOWN;
+    });
+  });
+
   const Styled = styled.div`
     button,
     a {
